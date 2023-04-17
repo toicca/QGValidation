0e9c292e09d2fa866a930b424442e2a4ce5fdf34
Kallonen Kimmo <kimmo.kallonen@helsinki.fi>
2023-04-17 16:44:04 +0300

    Add test exec

---
+++ scripts/config/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+OUT_ID="test"
+FILES="${COFFEAHOME}/data/UL17/test_HT.txt"
+
+python $COFFEAHOME/scripts/run_qg_analysis.py \
+    --out_id $OUT_ID \
+    --channel dijet \
+    --files $FILES \
+    --run mc \
+    --workers 1 \
+    --maxchunk 1
