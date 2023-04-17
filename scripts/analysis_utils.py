5819e47ed584429ec7f89dce925757dad3ce7807
Kallonen Kimmo <kimmo.kallonen@helsinki.fi>
2023-04-17 16:22:07 +0300

    Add analysis utils

---
+++ scripts/analysis_utils.py
@@ -0,0 +1,124 @@
+import sys
+import numpy as np
+
+data_names = ['DoubleMuon','ZeroBias']
+mc_names = ['DY','QCD_HT50to100','QCD_HT100to200','QCD_HT200to300','QCD_HT300to500','QCD_HT500to700','QCD_HT700to1000',
+            'QCD_HT1000to1500','QCD_HT1500to2000','QCD_HT2000toInf','QCD_Pt_15to30','QCD_Pt_30to50','QCD_Pt_50to80','QCD_Pt_80to120',
+            'QCD_Pt_120to170','QCD_Pt_170to300','QCD_Pt_300to470','QCD_Pt_470to600','QCD_Pt_600to800','QCD_Pt_800to1000',
+            'QCD_Pt_1000to1400','QCD_Pt_1400to1800','QCD_Pt_1800to2400','QCD_Pt_2400to3200','QCD_Pt_3200toInf', 'QCD_Pt-15to7000']
+
+def ObjSelection(obj,name,year=2016,selection=None):
+    '''Apply standard selection to ojects.
+    Inputs: 
+    obj: jagged_array to apply the selection
+    name: [jet,electron,muon,PF,tight_muon]
+    selection: bool mask for selection different than the standard
+
+    Returns: obj array with selection applied '''
+
+    if year != 2017 and year != 2016: #FixMe add other years
+        sys.exit("ERROR: Only 2016 and 2017 supported")
+
+    obj_list = ['jet','jet_dijet','electron','muon','PF','tight_muon','medium_muon']
+    
+    if name not in obj_list and selection == None:
+        sys.exit("ERROR: For non standard objects, provide a selection mask to be applied")
+
+    if selection != None:
+        return obj[selection]
+    else:
+        if year == 2016:
+            if name == 'jet':
+                selection = (
+                    (np.abs(obj.eta) < 2.4) & (obj["pt"] > 30)  & 
+                    (
+                        (obj["pt"] >= 50) 
+                        | ((obj["pt"] < 50) & (obj.pu_id>=6) )
+                    ) &
+                    ((obj.jetId==7)) & #tight
+                    (obj.mass > -1) 
+                )
+                
+            elif name == 'muon':
+                selection = (
+                    (np.abs(obj.eta) < 2.4) & (obj.pt > 10)  &
+                    (obj.dxy < 0.2) &
+                    (obj.dz < 0.5) &
+                    (obj.iso < 0.15) &
+                    (obj.isLoose) &
+                    (obj.mass > -1)  
+                )
+                
+            elif name == 'tight_muon':
+                selection = (
+                    (np.abs(obj.eta) < 2.4) & (obj.pt > 20)  &
+                    (obj.dxy < 0.2) &
+                    (obj.dz < 0.5) &
+                    (obj.iso < 0.15) &
+                    (obj.isTight) &
+                    (obj.mass > -1) 
+                )
+                
+            elif name == 'electron':
+                selection = (
+                    (np.abs(obj.eta) < 2.4) & (obj.pt > 15)  &
+                    (obj.iso < 0.15) & 
+                    (
+                        ((np.abs(obj.eta) < 1.4442) & (np.abs(obj.dxy) < 0.05) & (np.abs(obj.dz) < 0.1) ) 
+                        | ((np.abs(obj.eta) > 1.5660) & (np.abs(obj.dxy) < 0.1) & (np.abs(obj.dz) < 0.2) )
+                    ) & 
+                    (obj.cutbased >=2) &
+                    (obj.mass >-1)        
+                )
+                
+        if year == 2017:
+                
+            if name == 'jet':
+                selection = (
+                    # (np.abs(obj["eta"]) < 2.5) & #eta determined by b-tag SF limits for now
+                    (np.abs(obj["eta"]) < 4.7) &
+                    (obj["pt"] > 20)  & 
+                    (obj["jetId"]==6) & #tight
+                    (obj["mass"] > -1) 
+                )
+
+            elif name == 'muon':
+                selection = (
+                    (np.abs(obj["eta"]) < 2.3) &
+                    (obj["pt"] > 20) &
+                    (obj["iso"] < 0.15) &
+                    (obj["isTight"]) &
+                    (obj["mass"] > -1)  
+                )
+                
+            elif name == 'medium_muon':
+                selection = (
+                    (np.abs(obj["eta"]) < 2.3) & 
+                    (obj["pt"] > 20) &
+                    (obj["iso"] < 0.15) &
+                    (obj["isMedium"]) &
+                    (obj["mass"] > -1) 
+                )
+
+            elif name == 'tight_muon':
+                selection = (
+                    (np.abs(obj.eta) < 2.3) & 
+                    (obj["pt"] > 20) &
+                    (obj["iso"] < 0.15) &
+                    (obj["isTight"]) &
+                    (obj["mass"] > -1) 
+                )
+                
+            elif name == 'electron':
+                selection = (
+                    (np.abs(obj["eta"]) < 2.4) & (obj["pt"] > 10)  &
+                    (
+                        ((np.abs(obj["eta"]) < 1.4442) & (np.abs(obj["dxy"]) < 0.05) & (np.abs(obj["dz"]) < 0.1) ) 
+                        | ((np.abs(obj["eta"]) > 1.5660) & (np.abs(obj["dxy"]) < 0.1) & (np.abs(obj["dz"]) < 0.2) )
+                    ) & 
+                    (obj["cutbased"] >= 1) &
+                    (obj["mass"] > -1)        
+                )
+
+        return obj[selection], selection
+    
